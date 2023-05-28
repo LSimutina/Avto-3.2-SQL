17c4789d5e63ec72efb1dfc9b42b2ee134aec039
Lidiya <simutina.1994@mail.ru>
2023-05-28 16:15:06 +0300

    Initial commit

---
+++ src/test/java/ru/netology/page/LoginPage.java
@@ -0,0 +1,31 @@
+package ru.netology.page;
+
+
+import com.codeborne.selenide.SelenideElement;
+import ru.netology.data.DataHelper;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.page;
+
+public class LoginPage {
+    private SelenideElement loginField = $("[data-test-id=login] input");
+    private SelenideElement passwordField = $("[data-test-id=password] input");
+    private SelenideElement loginButton = $("[data-test-id=action-login]");
+    private SelenideElement errorLogin = $("[data-test-id='error-notification']");
+
+    public void verifyErrorLoginVisibility() {
+        errorLogin.shouldBe(visible);
+    }
+
+    public VerificationPage validLogin(DataHelper.AuthInfo info) {
+        login(info);
+        return page(VerificationPage.class);
+    }
+
+    public void login(DataHelper.AuthInfo info) {
+        loginField.setValue(info.getLogin());
+        passwordField.setValue(info.getPassword());
+        loginButton.click();
+    }
+}
